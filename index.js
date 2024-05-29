314c3add64a03fdd0c7d4e68ddf4df81a6c09410
juanpaolo delacruz <jpmd.creates@gmail.com>
2024-05-29 15:06:00 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, name){
+let matchingDrivers = drivers.filter(driver => driver.toUpperCase() === name.toUpperCase())
+       return matchingDrivers 
+}
+
+function fuzzyMatch(drivers, letters){
+    let firstLetter = drivers.filter(driver => driver.slice(0,2) === letters)
+    return firstLetter
+
+}
+function matchName(drivers, name){
+    let matchingNames = drivers.filter(driverObj => driverObj.name === name)
+    return matchingNames
+}
